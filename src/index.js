1376443e35039a3ae7714b5e53cd62fcc25da343
Luna Campos <lunacampos@Lunas-MacBook-Pro.local>
2020-04-08 19:39:43 -0700

    async/await

--- src/index.js
+++ src/index.js
@@ -9,61 +9,67 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/users', (request, response)=>{
+app.post('/users', async (request, response)=>{
   const user = new User(request.body);
-  user.save().then(()=>{
-    response.status(201).send(user);
-  }).catch((error)=>{
-    response.status(400).send(error);
-  })
+  try {
+    await user.save()
+    response.status(201).send(user)
+  } catch (error) {
+    response.status(400).send(error)
+  }
 })
 
-app.get('/users', (request, response)=>{
-  User.find({}).then((users)=>{
-    response.send(users)
-  }).catch((error)=>{
+app.get('/users', async (request, response)=>{
+  try {
+    const users = await User.find({})
+    response.send(users);
+  } catch (error) {
+    response.status(500).send();
+  }
+})
+
+app.get('/users/:id', async (request, response)=>{
+  const _id = request.params.id;
+
+  try {
+    const user = await User.findById(_id)
+    if(!user) return response.status(404).send()
+    response.send(user)
+  } catch (error){
     response.status(500).send()
-  })
-})
+  }
 
-app.get('/users/:id', (request, response)=>{
-  const _id = request.params.id;
-
-  User.findById(_id).then((user)=>{
-    if(!user){
-      return response.status(404).send()
-    }
-    response.send(user);
-  }).catch((error)=>{
-    response.status(500).send();
-  })
 })
 
 
-app.post('/tasks', (request, response)=>{
+app.post('/tasks', async (request, response)=>{
   const task = new Task(request.body);
-  task.save().then(()=>{
+  try {
+    await task.save();
     response.status(201).send(task);
-  }).catch((error)=>{
+  } catch (error){
     response.status(400).send(error);
-  })
+  }
+
 })
 
-app.get('/tasks/', (request, response)=>{
-  Task.find({}).then((tasks)=>{
-    response.send(tasks);
-  }).catch((error)=>{
+app.get('/tasks/', async (request, response)=>{
+  try {
+    const tasks = await Task.find({})
+    response.send(tasks)
+  } catch(error){
     response.status(500).send(error)
-  })
+  }
 })
-app.get('/tasks/:id', (request, response)=>{
+app.get('/tasks/:id', async (request, response)=>{
   const _id = request.params.id;
-  Task.findById(_id).then((task)=>{
+  try {
+    const task = await Task.findById(_id)
     if(!task) return response.status(404).send();
-    response.status(201).send(task);
-  }).catch((error)=>{
+    response.send(task)
+  } catch (error){
     response.status(500).send();
-  })
+  }
 })
 
 
