d50a4e11a4ff4e3bbfbc714add57a4ed0e758c35
Luna Campos <lunacampos@Lunas-MacBook-Pro.local>
2020-04-08 21:38:48 -0700

    router

--- src/index.js
+++ src/index.js
@@ -1,76 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('./db/mongoose');
-const User = require('./models/user')
-const Task = require('./models/task');
+const userRouter = require('./routers/user');
+const taskRouter = require('./routers/task')
+
 
 const app = express();
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(userRouter);
+app.use(taskRouter);
+const router = new express.Router()
 
-app.post('/users', async (request, response)=>{
-  const user = new User(request.body);
-  try {
-    await user.save()
-    response.status(201).send(user)
-  } catch (error) {
-    response.status(400).send(error)
-  }
-})
-
-app.get('/users', async (request, response)=>{
-  try {
-    const users = await User.find({})
-    response.send(users);
-  } catch (error) {
-    response.status(500).send();
-  }
-})
-
-app.get('/users/:id', async (request, response)=>{
-  const _id = request.params.id;
-
-  try {
-    const user = await User.findById(_id)
-    if(!user) return response.status(404).send()
-    response.send(user)
-  } catch (error){
-    response.status(500).send()
-  }
-
-})
-
-
-app.post('/tasks', async (request, response)=>{
-  const task = new Task(request.body);
-  try {
-    await task.save();
-    response.status(201).send(task);
-  } catch (error){
-    response.status(400).send(error);
-  }
-
-})
-
-app.get('/tasks/', async (request, response)=>{
-  try {
-    const tasks = await Task.find({})
-    response.send(tasks)
-  } catch(error){
-    response.status(500).send(error)
-  }
-})
-app.get('/tasks/:id', async (request, response)=>{
-  const _id = request.params.id;
-  try {
-    const task = await Task.findById(_id)
-    if(!task) return response.status(404).send();
-    response.send(task)
-  } catch (error){
-    response.status(500).send();
-  }
-})
 
 
 app.listen(port, ()=>{
